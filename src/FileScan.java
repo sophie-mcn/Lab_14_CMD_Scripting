2b6cde44346e7255e71229620bca9ba397353a72
sophie-mcn <mcnamask@mail.uc.edu>
2025-12-11 12:07:43 -0500

    trying something

--- src/FileScan.java
+++ src/FileScan.java
@@ -1,13 +1,63 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import javax.swing.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import static java.nio.file.StandardOpenOption.CREATE;
+
+void main(String[] args) {
+    JFileChooser chooser = new JFileChooser();
+    File selectedFile;
+    String rec = "";
+
+    if(args.length>0)
+    {
+      selectedFile = new File(args[0]);
+      if (!selectedFile.exists())
+      {
+        JOptionPane.showMessageDialog(null,"Please select a file");
+      }
     }
-}
+    else {
+        try {
+            File workingDir = new File(System.getProperty("user.dir"));
+            chooser.setCurrentDirectory(workingDir);
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                selectedFile = chooser.getSelectedFile();
+                Path file = selectedFile.toPath();
+
+                InputStream in =
+                        new BufferedInputStream(Files.newInputStream(file, CREATE));
+                BufferedReader reader =
+                        new BufferedReader(new InputStreamReader(in));
+
+
+                int line = 0;
+                int wordcount = 0;
+                int charcount = 0;
+
+                while ((rec = reader.readLine()) != null) {
+
+                    System.out.println(rec);
+                    line++;
+                    charcount += rec.length();
+                    String words[] = rec.split("\\s+");
+                    wordcount += words.length;
+                }
+                reader.close();
+
+                System.out.println("\nYou opened the file: " + selectedFile.getName());
+                System.out.println("There are " + line + " lines in the file.");
+                System.out.println("There are " + wordcount + " words in the file.");
+                System.out.println("There are " + charcount + " characters in the file.");
+
+            } else {
+                System.out.println("No file selected ... exiting. \nRun the program again and select a file.");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }
+
